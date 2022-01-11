1b932972e667195541904ddffcc30d1124f33bc6
Glee Albert <gleealbert307@gmail.com>
2022-01-11 11:57:40 +0100

    custom color and card

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+
+//
+
+import 'package:flutter/material.dart';
+
+const  kPrimaryColor = Color(0xff025024);
+const kButtonColor = Color(0xfff98c00);
--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -1,15 +1,14 @@
-
+import 'package:best_practics/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -17,36 +16,118 @@ class HomeScreen extends StatelessWidget {
             Text(
               'Home Page',
               style: GoogleFonts.lato(
-                fontWeight: FontWeight.bold,
-                fontSize: 25.0,
-                letterSpacing: 1.0,
-                color: Colors.black
-                
-              ),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.0,
+                  letterSpacing: 1.0,
+                  color: Colors.black),
             ),
-
             const Icon(
               Icons.account_circle,
               size: 30.0,
               color: Colors.grey,
-
             )
           ],
         ),
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-      
       body: Container(
-        margin: EdgeInsets.zero,
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
         child: Column(
           children: [
             Container(
+              height: 50.0,
               margin: EdgeInsets.zero,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
 
-              child: Row(
-                children: const [
-                  Icon(Icons.search)
+              decoration: BoxDecoration(
+                color: Colors.blueGrey[50],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.search),
+                  hintText: 'Search',
+                  suffixIcon: Icon(Icons.mic),
+                ),
+                style: GoogleFonts.lato(
+                    fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),            
+            ),
+            const SizedBox(height: 10.0,),
+
+            Container(
+              margin: EdgeInsets.zero,
+              padding: const EdgeInsets.symmetric(horizontal: 10 , vertical: 10),
+              height: 200.0,
+              width: double.infinity,
+              
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: kPrimaryColor
+              ),
+
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Card Studio',
+
+                        style: GoogleFonts.pacifico(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.white
+                        ),
+                      ),
+                      Text(
+                        'Tell me your name',
+
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 20,
+                          color: Colors.white
+                        ),
+                      ),
+                      Text(
+                        'Glee Albert',
+
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w100,
+                          fontSize: 16,
+                          color: Colors.white
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Container(
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: kButtonColor
+                    ),
+                    child: TextButton(
+                      onPressed: (){},
+            
+                      child: Text(
+                        'Bundle',
+
+                        style: GoogleFonts.lato(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.white
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+
                 ],
               ),
             )
